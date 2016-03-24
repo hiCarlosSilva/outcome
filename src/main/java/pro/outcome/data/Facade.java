6c9a27fb0cc5592922305e79093c5d14bcef7e35
Carlos Silva <carlos@connector.im>
2016-03-24 18:18:18 +0000

    Corrected standard header message

--- src/main/java/pro/outcome/data/Facade.java
+++ src/main/java/pro/outcome/data/Facade.java
@@ -1,7 +1,7 @@
-// All the information contained in this source code file is a property of Intersection Labs Limited,
+// All the information contained in this source code file is a property of Outcome Professional Services Limited,
 // a company registered in the United Kingdom. Use and distribution of any part of the information 
 // contained in this source code file without our prior consent is forbidden. If you have an interest 
-// in using any part of this source code in your software, please contact us on listening@connector.im.
+// in using any part of this source code in your software, please contact hiCarlosSilva@gmail.com.
 package pro.outcome.data;
 import java.lang.reflect.ParameterizedType;
 import java.util.Map;
